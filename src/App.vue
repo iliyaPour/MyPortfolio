9967d4bb9b1cd4732c11130e6f9442391a6e32a1
iliyaPour <iliya.pourzanjani6@gmail.com>
2026-08-15 19:00:45 +0330

    First Look Of Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
-<script setup></script>
+<script setup>
+import Navbar from "./components/layout/Navbar.vue";
+
+
+</script>
 
 <template>
-  <div class="min-h-screen bg-slate-900 flex items-center justify-center">
-    <h1 class="text-4xl font-bold text-teal-400">
-      سلام! تیلویند ۴ با موفقیت نصب شد 🎉
-    </h1>
-  </div>
+  <Navbar />
 </template>
 
 <style scoped>
