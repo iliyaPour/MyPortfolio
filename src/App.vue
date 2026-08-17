37e7fc408f86ce32e8a67f6b57cb5d7e6293d714
iliyaPour <iliya.pourzanjani6@gmail.com>
2026-08-17 10:11:03 +0330

    first look of Footer

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <script setup>
 import Navbar from "./components/layout/Navbar.vue";
-
-
+import Footer from "./components/layout/Footer.vue";
 </script>
 
 <template>
   <Navbar />
+
+  <Footer />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,3 +1,14 @@
 <template>
-
+  <footer class="bg-light-secondary dark:bg-dark-secondary py-12">
+    <div class="container mx-auto">
+      <div class="flex flex-col md:flex-row space-y-6 lg:space-y-0 items-center justify-between">
+        <div class="flex space-x-6 items-center justify-center">
+          <img class="h-8 w-auto" src="/src/assets/images/dawg.png" alt="logo" />
+        </div>
+        <p class="text-paragraph opacity-80 text-[15px]">&copy;تمام حقوق محفوظ است</p>
+      </div>
+    </div>
+  </footer>
 </template>
+
+<script></script>
